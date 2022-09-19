9cc2eeaf491ed2d37906595bed3e849fa57bd9a6
Jose Flavio <zflavio@ufcg.edu.br>
2022-09-19 19:34:19 -0300

    React Developer Bootcamp - Challenge - Calculator with Functional Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Content, Row, Column } from "./styles";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [currentNumber, setCurrentNumber] = useState('0'); //estado inicial 0
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [currentOperator, setCurrentOperator] = useState('');
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${currentNumber === '0' ? '' : prev}${number}`);
+  }
+
+  const handleOnClear = () => {
+    clearDisplay();
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setCurrentOperator('');
+  }
+
+  const clearDisplay = () => {
+    setCurrentNumber('');
+  }
+
+  const handleOperation = (operator) => {
+    if( firstNumber === '0' ){
+      setFirstNumber(currentNumber);
+      clearDisplay();
+    }
+    setCurrentOperator(operator);
+  }
+
+  const handleOnEquals = () => {
+    switch( currentOperator ) {
+      case '+' :
+        setCurrentNumber(parseInt(firstNumber) + parseInt(currentNumber));
+        setFirstNumber('0');
+        break;
+      case '-' :
+        setCurrentNumber(parseInt(firstNumber) - parseInt(currentNumber));
+        setFirstNumber('0');
+        break;
+      case '*' :
+        setCurrentNumber(parseInt(firstNumber) * parseInt(currentNumber));
+        setFirstNumber('0');
+        break;
+      case '/' :
+        setCurrentNumber(parseInt(firstNumber) / parseInt(currentNumber));
+        setFirstNumber('0');
+        break;
+      default :
+        setCurrentOperator("");
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+          <Row>
+            <Button label={"1"} onClick={() => handleAddNumber("1")}/>
+            <Button label={2} onClick={() => handleAddNumber("2")}/>
+            <Button label={3} onClick={() => handleAddNumber("3")}/>
+            <Button label={"+"} onClick={() => handleOperation("+")}/>
+          </Row>
+          <Row>
+            <Button label={"4"} onClick={() => handleAddNumber("4")}/>
+            <Button label={5} onClick={() => handleAddNumber("5")}/>
+            <Button label={6} onClick={() => handleAddNumber("6")}/>
+            <Button label={"-"} onClick={() => handleOperation("-")}/>
+          </Row>
+          <Row>
+            <Button label={"7"} onClick={() => handleAddNumber("7")}/>
+            <Button label={8} onClick={() => handleAddNumber("8")}/>
+            <Button label={9} onClick={() => handleAddNumber("9")}/>
+            <Button label={"*"} onClick={() => handleOperation("*")}/>
+          </Row>
+          <Row>
+            <Button label={"C"} onClick={handleOnClear}/>
+            <Button label={0} onClick={() => handleAddNumber("0")}/>
+            <Button label={"/"} onClick={() => handleOperation("/")}/>
+            <Button label={"="} onClick={handleOnEquals}/>
+          </Row>
+      </Content>
+       
+    </Container>
   );
 }
 
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 28px;
+    font-family: 'Roboto';
+
+    input {
+        font-size: 30px;
+        font-family: 'Roboto';
+        width: 100%;
+        height: 75px;
+        background-color: #EAEAEA;
+        border: 0;
+        padding: 0 10px;
+        
+        text-align: right;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    }
+        
+`
